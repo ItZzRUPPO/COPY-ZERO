add3510880fb40347780d8c154ca9df89a2da085
DEVSMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-07-17 15:07:11 +0300

    Create time.js

---
+++ commands/More/time.js
@@ -0,0 +1,48 @@
+
+const ms = require ('ms')
+
+client.on('message', message => {
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+
+    const command = args.shift().toLowerCase();
+
+ 
+
+    if(command === "timer"){
+
+        let Timer = args[0];
+
+ 
+
+ 
+
+        if(!args[0]){
+
+            return message.channel.send("Usage: prefix + timer + Duration + s|m|h")
+
+        }
+
+ 
+
+        if(args[0] <= 0){
+
+            return message.channel.send("Usage: prefix + timer + Duration + s|m|h")
+
+        }
+
+ 
+
+        message.channel.send("Time is start:"+ ms(ms(Timer), {long: true}))
+
+        setTimeout(function(){
+
+ 
+
+          message.channel.send(message.author.toString()+ `Time is Done,: ${ms(ms(Timer), {long: true})}`)
+
+        }, ms(Timer));
+
+    }
+
+});
