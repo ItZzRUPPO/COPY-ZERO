285b59adefcb9d29fa96dda62e13d44a58b2772c
DEVSMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-07-17 18:03:45 +0300

    Add files via upload

--- commands/fun/rate.js
+++ commands/fun/rate.js
@@ -1,25 +1,25 @@
+const fs = require("fs");
 const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
-const { Prefix } = require("../../config.js");
 
 module.exports = {
   name: "rate",
-  aliases: ["Rate"],
-  description: "Bot Rate Your Given Thing!",
-  usage: "Rate <Text>",
-  run: async (client, message, args) => {
-   
+  aliases: [""],
+  description: "Help Command!",
+  usage: "o/Help | <Command Name>",
+  run: async(client, message, args) => {
+
+    
     let Content = args.join(" ");
 
     if (!Content)
-      return message.channel.send(`Please Give Me Something To Rate!`);
+      return message.channel.send(`Please give me something to rate!`);
 
     let embed = new Discord.MessageEmbed()
-      .setColor("RED")
+      .setColor("")
       .setTitle(`I Rate`)
       .setDescription(`${Math.floor(Math.random() * 11)}/10 To ${Content}`)
-      .setFooter(`Requested by ${message.author.username}`)
-      .setTimestamp();
+      
 
     message.channel.send(embed);
 
