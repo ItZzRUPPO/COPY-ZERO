eaa93a4be03d992e0a6fe7ca4bdfcafd530e132c
DEVSMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-07-17 16:05:11 +0300

    Update haaama.js

--- haaama.js
+++ haaama.js
@@ -30,7 +30,7 @@ client.on("message", async message => {
   }
 });
 
-let modules = ["fun", "info", "moderation", "gif", "photo","More"];
+let modules = ["fun", "info", "moderation", "gif", "photo"];
 
 modules.forEach(function(module) {
   fs.readdir(`./commands/${module}`, function(err, files) {
@@ -345,7 +345,37 @@ const smsm = message.member;
 smsm.roles.add('Muted');
 }})
 
+const ms = require ('ms')
 
+let Timer = args[0];
+
+  if(!args[0]){
+
+    return message.channel.send(":x: " + "| Please Enter a time period followed by \"s or m or h\"");
+
+  }
+
+  if(args[0] <= 0){
+
+    return message.channel.send(":x: " + "| Please Enter a time period followed by \"s or m or h\"");
+
+  }
+
+  message.channel.send(":white_check_mark: " + "<a:emoji_200:865930365306667029>""`| Timer Started For  " + `${ms(ms(Timer), {long: true})}`)
+
+  setTimeout(function(){
+
+    message.channel.send(message.author.toString() " + "<a:Yes:863080680496562196>""| The Timer Has FINISHED"!, it lasted: ${ms(ms(Timer), {long: true})}`)
+
+  }, ms(Timer));
+
+}
+
+module.exports.help = {
+
+    name: "timer"
+
+}
  
 
         
