d47a3795fdb3539e25c1735dd2e851ea1cc1c97c
DEVSMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-07-17 18:54:26 +0300

    Update haaama.js

--- haaama.js
+++ haaama.js
@@ -480,29 +480,15 @@ client.on('message',async message => {
 
   })
     
-exports.run = (client, message, args) => { 
+
 
   
 
   
 
-  if (!args[0]) {
 
-        return message.channel.send(`https://google.com`);
 
-    }
 
-    message.channel.send(`http://www.google.com/search?q=${args.join('+')}&pws=0`);
-
-  
-
-}
-
-exports.help = {
-
-  name: 'google'
-
-}
 
 
 
