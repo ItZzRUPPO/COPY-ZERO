5840b9469681592c24e1b525492ca2c2741bf282
DEVSMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-07-17 13:28:50 +0300

    Update haaama.js

--- haaama.js
+++ haaama.js
@@ -331,21 +331,9 @@ client.on("message", message => {
   }
 });
 
-const badword = ["quz", "dakt","kerm","quzi","xushkt"];// baddle xow wsha bxara am kawanaya
 
- client.on('message',async message => {
 
-if (badword.some(d => message.content.toLowerCase().includes(d))) {
 
-message.delete();
-
-message.reply(`No mistakes fucker`);
-
-const bawan = message.member;
-
-bawan.roles.add('Muted');
-
- }})
 
 
   
