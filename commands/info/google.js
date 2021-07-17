256d5b8977e6654e86a925aefc308351cb3e8832
DEVSMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-07-17 12:55:12 +0300

    Create google.js

---
+++ commands/info/google.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+exports.run = (client, message, args) => { 
+
+  
+
+  
+
+  if (!args[0]) {
+
+        return message.channel.send(`https://google.com`);
+
+    }
+
+    message.channel.send(`http://www.google.com/search?q=${args.join('+')}&pws=0`);
+
+  
+
+}
+
+exports.help = {
+
+  name: 'google'
+
+}
