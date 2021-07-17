603ef6b91660f937e93af475b154d1437615a3f1
DEVSMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-07-17 21:03:21 +0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -13,39 +13,39 @@ module.exports = {
     
     let embed = new MessageEmbed()
     .setColor("RANDOM")
-    .setTitle(`Help oPoRo!`)
+    .setTitle(`Help ZERO!`)
 
     .setDescription(`
  
-<a:king:864136523410571295>︙**Info** 
+<a:king:864136523410571295> ︙**Info** 
 \`help\`, \`ping\`, \`Server\`,
 \`userinfo\`, \`invite\`, \`uptime\`
 \`say\`, \`uinvite\`, \`roles\`
 \`stats\`, \`listemoji\`, \`se\`,
 \`weather\`,\`timer\`,\`avatar\`
 
-<a:ban:863095808572522547>︙**Moderation** 
+<a:ban:863095808572522547> ︙**Moderation** 
 \`ban\`, \`unban\`, \`kick\`,
 \`mute\`, \`unmute\`, \`clear\`
 \`lock\`, \`unlock\`, \`slowmode\`
 \`nick\`, \`hide\`, \`unhide\` 
 \`vmute\`,\`vunmute\`,\`vkick\`
 
-<a:3HyperBlob:863095794570362919>︙**Fun**
+<a:3HyperBlob:863095794570362919> ︙**Fun**
 \`ascii\`, \`iq\`, \`rps\`,
 \`howgay\`, \`rate\`
 
-:-<a:gifland_tatlisbeyaz:864551590354223104>︙**Gifs**
+<a:gifland_tatlisbeyaz:864551590354223104> ︙**Gifs**
 \`boy\`, \`girl\`, \`anime\`,
 \`smoke\`, \`baby\`, \`love\`,
 \`neon\`
 
-<a:gifland_kedicikbeyaz:792364864089358346>︙**Photos** 
+<a:gifland_kedicikbeyaz:792364864089358346> ︙**Photos** 
 \`Pboy\`, \`Pgirl\`, \`Panime\`,
 \`Pcartoon\`, \`Pbaby\`, \`Plove\`,
 \`Psmoke\`
 
-<a:link:864142531758260234>︙**Links**
+<a:link:864142531758260234> ︙**Links**
 [Server](https://discord.gg/yyJKH4NGuQ)**-**[Invite](https://discord.com/api/oauth2/authorize?client_id=830265742795866162&permissions=8&scope=bot)
 
 `)  
