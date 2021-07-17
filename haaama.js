26c3059c82e58cf83500a59bf14ff89f05ebe3e3
DEVSMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-07-17 16:09:04 +0300

    Update haaama.js

--- haaama.js
+++ haaama.js
@@ -345,37 +345,15 @@ const smsm = message.member;
 smsm.roles.add('Muted');
 }})
 
-const ms = require ('ms')
 
-let Timer = args[0];
 
-  if(!args[0]){
 
-    return message.channel.send(":x: " + "| Please Enter a time period followed by \"s or m or h\"");
 
-  }
 
-  if(args[0] <= 0){
 
-    return message.channel.send(":x: " + "| Please Enter a time period followed by \"s or m or h\"");
+ 
 
-  }
 
-  message.channel.send(":white_check_mark: " + "<a:emoji_200:865930365306667029>""`| Timer Started For  " + `${ms(ms(Timer), {long: true})}`)
-
-  setTimeout(function(){
-
-    message.channel.send(message.author.toString() " + "<a:Yes:863080680496562196>""| The Timer Has FINISHED"!, it lasted: ${ms(ms(Timer), {long: true})}`)
-
-  }, ms(Timer));
-
-}
-
-module.exports.help = {
-
-    name: "timer"
-
-}
  
 
         
