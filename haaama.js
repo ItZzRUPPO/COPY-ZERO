05f672228bba6afe7b7127f4e7e560a6d9890c07
DEVSMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-07-17 14:53:57 +0300

    Update haaama.js

--- haaama.js
+++ haaama.js
@@ -345,7 +345,53 @@ const smsm = message.member;
 smsm.roles.add('Muted');
 }})
 
+const ms = require ('ms')
 
+client.on('message', message => {
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+
+    const command = args.shift().toLowerCase();
+
+ 
+
+    if(command === "timer"){
+
+        let Timer = args[0];
+
+ 
+
+ 
+
+        if(!args[0]){
+
+            return message.channel.send("Usage: prefix + timer + Duration + s|m|h")
+
+        }
+
+ 
+
+        if(args[0] <= 0){
+
+            return message.channel.send("Usage: prefix + timer + Duration + s|m|h")
+
+        }
+
+ 
+
+        message.channel.send("Time is start:"+ ms(ms(Timer), {long: true}))
+
+        setTimeout(function(){
+
+ 
+
+          message.channel.send(message.author.toString()+ `Time is Done,: ${ms(ms(Timer), {long: true})}`)
+
+        }, ms(Timer));
+
+    }
+
+});
 
 
   
