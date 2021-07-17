5b9b858ac6311534b09fe8d1d4706ef43034fb19
DEVSMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-07-17 17:55:58 +0300

    Update haaama.js

--- haaama.js
+++ haaama.js
@@ -483,37 +483,7 @@ client.on('message',async message => {
 
 
 
-const ms = require ('ms')
-  name: "respect",
-
-  aliases: ["resp"],
-
-  description: "Help Command!",
-
-  usage: "o/Help | <Command Name>",
-
-  run: async(client, message, args) => {
-
-    let embed = new Discord.MessageEmbed()
-
-      .setColor("")
-
-      .setFooter(`Press F to pay respect`)
-
-      .setDescription(`${message.member} has paid their respect${args.length ? ` to ${args.join(' ')}.` : ''}`)
-
-    
-
-message.channel.send(embed);
-
-    await message.delete().catch(() => null);
-
-    return message.react("Ⓕ")
-
-  }
-
-};
-
+ 
 
 
 
