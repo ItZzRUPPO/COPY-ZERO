9af1ce66d7bc125b8c8818f13bbd6b807f5f0a52
DEVSMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-07-19 14:43:42 +0300

    Update haaama.js

--- haaama.js
+++ haaama.js
@@ -13,8 +13,8 @@ const prefix = "-";
 client.on("ready", async () => {
   console.log(`ready!`);
   client.user
-       client.setActivity(`${PREFIX}help | Server ${client.guilds.cache.size},Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "PLAYING"});
-       client.setActivity(`${PREFIX}help | By SMSM`, { type: "PLAYING"})
+       .setActivity(`${Prefix}help |By SMSM ${client.guilds.cache.size} Server,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "PLAYING" })
+       .setActivity(`By SMSM`)
 
   .catch(error => console.log(error));
 });
