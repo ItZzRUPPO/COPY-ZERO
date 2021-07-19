d099a275971cd2048ad7d3142382d96a319aa741
DEVSMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-07-20 01:13:15 +0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -18,31 +18,31 @@ module.exports = {
     .setDescription(`
  
 <a:king:864136523410571295> ︙**Info** 
-\`help\`- \`ping\`- \`Server\`-\`id\`
-\`userinfo\`- \`invite\`- \`uptime\`
-\`say\`- \`uinvite\`- \`roles\`-\`se\`
-\`stats\`-\`avatar\`- \`support\`
-\`weather\`-\`timer\`- \`members\`
+\`help\` - \`ping\` - \`Server\` - \`id\`
+\`userinfo\` - \`invite\` - \`uptime\`
+\`say\` - \`uinvite\` - \`roles\` - \`se\`
+\`stats\` - \`avatar\` - \`support\`
+\`weather\` - \`timer\` - \`members\`
 
 <a:ban:863095808572522547> ︙**Moderation** 
-\`ban\`- \`unban\`- \`kick\`,
-\`mute\`- \`unmute\`- \`clear\`
-\`lock\`- \`unlock\`- \`slowmode\`
-\`nick\`- \`hide\`- \`unhide\` 
-\`vmute\`-\`vunmute\`-\`vkick\`
+\`ban\` - \`unban\` - \`kick\`,
+\`mute\` - \`unmute\` - \`clear\`
+\`lock\` - \`unlock\` - \`slowmode\`
+\`nick\` - \`hide\` - \`unhide\` 
+\`vmute\` - \`vunmute\` -\`vkick\`
 
 <a:3HyperBlob:863095794570362919> ︙**Fun**
-\`ascii\`- \`iq\`- \`rps\`
-\`howgay\`- \`rate\`
+\`ascii\` - \`iq\` - \`rps\`
+\`howgay\` - \`rate\`
 
 <a:run:866009756124446780> ︙**Gifs**
-\`boy\`- \`girl\`- \`anime\`
-\`smoke\`- \`baby\`- \`love\`
+\`boy\` - \`girl\` - \`anime\`
+\`smoke\` - \`baby\` - \`love\`
 \`neon\`
 
 <a:hi:866009710800928788> ︙**Photos** 
-\`Pboy\`- \`Pgirl\`- \`Panime\`,
-\`Pcartoon\`- \`Pbaby\`- \`Plove\`,
+\`Pboy\` - \`Pgirl\` - \`Panime\`,
+\`Pcartoon\` - \`Pbaby\` - \`Plove\`,
 \`Psmoke\`
 
 <a:link:864142531758260234> ︙**Links**
