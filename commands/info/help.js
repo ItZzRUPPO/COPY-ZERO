e18e859bfea2f5bb3adf900397e6a52f83f73317
DEVSMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-07-20 20:29:00 +0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -15,14 +15,14 @@ module.exports = {
     .setColor("RANDOM")
     .setTitle(`Zero Help`)
 
-    .setDescription(`
+    .setDescription(`My Prefix is *-*
  
 <:emoji_213:866853582866874378> ︙**Info** 
 \`help\` - \`ping\` - \`Server\` - \`id\`
 \`userinfo\` - \`invite\` - \`uptime\`
 \`say\` - \`uinvite\` - \`roles\` - \`se\`
 \`stats\` - \`avatar\` - \`support\`
-\`weather\` - \`timer\` - \`members\`
+\`weather\` - \`timer\` - \`members\` - \`website\`
 
  <:HAMER:866805517975814184> ︙**Moderation** 
 \`ban\` - \`unban\` - \`kick\`,
@@ -41,12 +41,12 @@ module.exports = {
 \`neon\`
 
 <:lovy:866841502272454717> ︙**Photos** 
-\`Pboy\` - \`Pgirl\` - \`Panime\`,
-\`Pcartoon\` - \`Pbaby\` - \`Plove\`,
+\`Pboy\` - \`Pgirl\` - \`Panime\`
+\`Pcartoon\` - \`Pbaby\` - \`Plove\`
 \`Psmoke\`
 
 <:partner:863110940257550336> ︙**Links**
-[Server](https://discord.gg/yyJKH4NGuQ)**-**[Invite](https://discord.com/api/oauth2/authorize?client_id=830265742795866162&permissions=8&scope=bot)
+[Server](https://discord.gg/yyJKH4NGuQ)**-**[Invite](https://discord.com/api/oauth2/authorize?client_id=830265742795866162&permissions=8&scope=bot)**-**[Website](https://zero-bot-system-.glitch.me)
 
 `)  
    .setFooter(`${message.author.username}`)
