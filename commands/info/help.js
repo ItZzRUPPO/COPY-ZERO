6bda6eaa7b43ae137748a0a05074f6df93134824
SMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-09-14 02:33:41 +0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -17,36 +17,36 @@ module.exports = {
 
     .setDescription(`My Prefix is *-*
  
-<:emoji_213:866853582866874378> ︙**Info** 
+**Info** 
 \`help\` - \`ping\` - \`Server\` - \`id\`
 \`userinfo\` - \`invite\` - \`uptime\`
 \`say\` - \`uinvite\` - \`roles\` - \`se\`
 \`stats\` - \`avatar\` - \`support\`
 \`weather\` - \`timer\` - \`members\`
 \`website\`
- <:HAMER:866805517975814184> ︙**Moderation** 
+**Moderation** 
 \`ban\` - \`unban\` - \`kick\`,
 \`mute\` - \`unmute\` - \`clear\`
 \`lock\` - \`unlock\` - \`slowmode\`
 \`nick\` - \`hide\` - \`unhide\` 
 \`vmute\` - \`vunmute\` -\`vkick\`
 
-<:fire:866843047098187816> ︙**Fun**
+**Fun**
 \`ascii\` - \`iq\` - \`rps\`
 \`howgay\` - \`rate\`
 
-<:Cute:866821599562170398> ︙**Gifs**
+**Gifs**
 \`boy\` - \`girl\` - \`anime\`
 \`smoke\` - \`baby\` - \`love\`
 \`neon\`
 
-<:lovy:866841502272454717> ︙**Photos** 
+**Photos** 
 \`Pboy\` - \`Pgirl\` - \`Panime\`
 \`Pcartoon\` - \`Pbaby\` - \`Plove\`
 \`Psmoke\`
 
-<:partner:863110940257550336> ︙**Links**
-[Server](https://discord.gg/yyJKH4NGuQ)**-**[Invite](https://discord.com/api/oauth2/authorize?client_id=830265742795866162&permissions=8&scope=bot)**-**[Website](https://zero-system.glitch.me)
+**Links**
+[Server](https://discord.gg/vcK3BNtrNa)**-**[Invite](https://discord.com/api/oauth2/authorize?client_id=830265742795866162&permissions=8&scope=bot)**-**[Website](https://zero-system.glitch.me)
 
 `)  
    .setFooter(`${message.author.username}`)
