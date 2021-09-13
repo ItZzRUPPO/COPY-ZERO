d584cd62b5c89f00314ba88c33e5ae4d8345e6cc
SMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-09-14 02:54:04 +0300

    Update haaama.js

--- haaama.js
+++ haaama.js
@@ -89,7 +89,7 @@ client.on("message", async message => {
 });
 
 
-const channelid = "861737765561696267"    //id channele vc
+const channelid = "886944896991232000"    //id channele vc
 client.on("ready", () => {
     const channel = client.channels.cache.get(channelid);
     if (!channel) return console.error("The channel does not exist!");
