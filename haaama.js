0af9b7687493375116c6bba03a72225e36764df5
SMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-10-01 20:15:39 +0300

    Update haaama.js

--- haaama.js
+++ haaama.js
@@ -13,7 +13,7 @@ const prefix = "-";
 client.on("ready", async () => {
   console.log(`ready!`);
   client.user
-       .setActivity(`${Prefix}help | ${client.guilds.cache.size} Server,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "STREAMING" })
+       .setActivity(`${Prefix}help | ${client.guilds.cache.size} Server,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "PLAYING" })
        .setActivity(`By SMSM`)
 
   .catch(error => console.log(error));
